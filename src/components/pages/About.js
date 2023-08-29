369b1fc792ac981bfcb170e838147289bd01ad99
DiogoS77 <diogo.messi.18@hotmail.com>
2023-08-29 15:04:31 +0100

    adjusted dark, light emoji to be centered

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -21,35 +21,51 @@ export default function Home() {
   };
 
   return (
-    <div
-      className={`about-container ${isDarkMode ? "dark-mode" : "light-mode"}`}
-    >
-      <button className="mode-switch" onClick={toggleDarkMode}>
-        {isDarkMode ? "🌞" : "🌙"}
-      </button>
-      <div className="text-container">
-        <h1>About Me</h1>
-        <div className={`typing-animation ${showText ? "visible" : "hidden"}`}>
-          <p>&#x1F44B; Hey, I'm Diogo, a Birmingham Coding Bootcamp student.</p>
-          <p>
-            I am currently studying Web Development and striving to make my mark
-            in the development world.
-          </p>
-          <p>
-            I am an inquisitive and hardworking individual. Coding intrigued me
-            due to the limitless challenges and opportunities it offers.
-          </p>
-          <p>
-            My primary goal is to push my boundaries and explore the endless
-            possibilities in this field. I am passionate about programming since
-            it's a domain where you can continuously learn and help others
-            learn.
-          </p>
+    <>
+      <div
+        className={`center-container ${
+          isDarkMode ? "dark-mode" : "light-mode"
+        }`}
+      >
+        <button className="mode-switch" onClick={toggleDarkMode}>
+          {isDarkMode ? "🌞" : "🌙"}
+        </button>
+      </div>
+      <div
+        className={`about-container ${isDarkMode ? "dark-mode" : "light-mode"}`}
+      >
+        <div className="text-container">
+          <h1>About Me</h1>
+          <div
+            className={`typing-animation ${showText ? "visible" : "hidden"}`}
+          >
+            <p>
+              &#x1F44B; Hey, I'm Diogo, a Birmingham Coding Bootcamp student.
+            </p>
+            <p>
+              I am currently studying Web Development and striving to make my
+              mark in the development world.
+            </p>
+            <p>
+              I am an inquisitive and hardworking individual. Coding intrigued
+              me due to the limitless challenges and opportunities it offers.
+            </p>
+            <p>
+              My primary goal is to push my boundaries and explore the endless
+              possibilities in this field. I am passionate about programming
+              since it's a domain where you can continuously learn and help
+              others learn.
+            </p>
+          </div>
+        </div>
+        <div className="image-container">
+          <img
+            src={Portrait}
+            alt="Diogo's Portrait"
+            className="profile-image"
+          />
         </div>
       </div>
-      <div className="image-container">
-        <img src={Portrait} alt="Diogo's Portrait" className="profile-image" />
-      </div>
-    </div>
+    </>
   );
 }
